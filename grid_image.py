ae90e742072d780233e4e529dff7cff513338bee
nk.benamara <nk.benamara@gmail.com>
2018-10-15 15:57:23 +0200

    third commit

--- grid_image.py
+++ grid_image.py
@@ -12,12 +12,10 @@ def image_to_grid(image,grid_size_h,grid_size_w,plot=False):
     grid_h = np.append(np.arange(0, int(h + h / grid_size_h), int(h / grid_size_h)), 0)
     grid_w = np.append(np.arange(0, int(w + w / grid_size_w), int(w / grid_size_w)), 0)
     count = 0
-    grid_sub=[]
     for i in range(0, len(grid_h) - 2):
         for j in range(0, len(grid_w) - 2):
             count += 1
             sub_I = I[grid_h[i]:grid_h[i + 1] - 1, grid_w[j]:grid_w[j + 1] - 1]
-            grid_sub.append(sub_I)
             if plot == True:
                 plt.subplot(len(grid_h) - 2, len(grid_w) - 2, count)
                 plt.axis('off')
@@ -26,5 +24,5 @@ def image_to_grid(image,grid_size_h,grid_size_w,plot=False):
             else:
                 pass
     plt.show()
-    return grid_sub
-grid= image_to_grid(I,16,16,True)
+
+image_to_grid(I,16,16,True)
